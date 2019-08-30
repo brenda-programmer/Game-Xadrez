191dd5c1e3982662c76f2ce338a4ca1a4cb8cce5
Brenda Oliveira <brenda.programmer@gmail.com>
2019-08-30 11:44:47 -0300

    Tratando erros com excecao personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,13 +8,21 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0)); // peça desejada na posição desejada no tabuleiro
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(Cor.Preta,tab), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0)); // peça desejada na posição desejada no tabuleiro
+                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 9));
+                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -18,10 +18,43 @@
         {
             return pecas[linha, coluna];
         }
+
+        public Peca peca(Posicao pos) // sobrecarga do método peca
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos) // método para verificar se existe peça em uma dada posição
+        {
+            validarPosicao(pos);// validar se a posição é válida, caso for executa a próxima linha, caso não lança uma exceção
+            return peca(pos) != null; // se for true significa que existe uma peça naquela posição
+        }
         public void colocarPeca(Peca p, Posicao pos)
         {
+            // só posso colocar uam peça onde não tenha nenhuma peça
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p; // a matriz de peças do tabuleiro recebe a peça que você declarar na posição desejada
-            p.posicao = pos;// essa peça declarada terá sua posição igual a posição desejada na matriz do tabuleiro
+            p.posicao = pos; // essa peça declarada terá sua posição igual a posição desejada na matriz do tabuleiro
+        }
+
+        public bool posicaoValida(Posicao pos) // método para testar se uma dada posição é valida ou não
+        {
+            if(pos.linha<0 || pos.linha>=linhas || pos.coluna<0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos) // método que recebe uma posição e, caso a posição não passe pelo método posicaoValida, lança uma excessão personalizada
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
